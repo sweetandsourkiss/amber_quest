aefd551a89a8600452badfe1a94828d363fe200a
sweetandsourkiss <mooul0529@naver.com>
2024-12-16 17:26:12 +0900

    Add collision, keyboard input logic

--- lib/actors/ember.dart
+++ lib/actors/ember.dart
@@ -1,10 +1,31 @@
 import 'dart:async';
 
+import 'package:amber_quest/actors/water_enemy.dart';
 import 'package:amber_quest/ember_quest.dart';
+import 'package:amber_quest/objects/ground_block.dart';
+import 'package:amber_quest/objects/platform_block.dart';
+import 'package:amber_quest/objects/star.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flutter/services.dart';
 
 class EmberPlayer extends SpriteAnimationComponent
-    with HasGameReference<EmberQuestGame> {
+    with KeyboardHandler, CollisionCallbacks, HasGameReference<EmberQuestGame> {
+  int horizontalDirection = 0;
+  final Vector2 velocity = Vector2.zero();
+  final double moveSpeed = 200;
+  final Vector2 fromAbove = Vector2(0, -1);
+  bool isOnGround = false;
+
+  final double gravity = 15;
+  final double jumpSpeed = 600;
+  final double terminalVelocity = 150;
+
+  bool hasJumped = false;
+
+  bool hitByEnemy = false;
+
   EmberPlayer({
     required super.position,
   }) : super(
@@ -25,6 +46,105 @@ class EmberPlayer extends SpriteAnimationComponent
           16,
         ),
       ),
+    );
+    add(CircleHitbox());
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    horizontalDirection = 0;
+    horizontalDirection += (keysPressed.contains(LogicalKeyboardKey.keyA) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowLeft))
+        ? -1
+        : 0;
+
+    horizontalDirection += (keysPressed.contains(LogicalKeyboardKey.keyD) ||
+            keysPressed.contains(LogicalKeyboardKey.arrowRight))
+        ? 1
+        : 0;
+    hasJumped = keysPressed.contains(LogicalKeyboardKey.space);
+    return true;
+  }
+
+  @override
+  void update(double dt) {
+    velocity.x = horizontalDirection * moveSpeed;
+    super.update(dt);
+    if (horizontalDirection < 0 && scale.x > 0) {
+      flipHorizontally();
+    } else if (horizontalDirection > 0 && scale.x < 0) {
+      flipHorizontally();
+    }
+
+    velocity.y += gravity;
+
+    if (hasJumped) {
+      if (isOnGround) {
+        velocity.y = -jumpSpeed;
+        isOnGround = false;
+      }
+      hasJumped = false;
+    }
+
+    velocity.y = velocity.y.clamp(-jumpSpeed, terminalVelocity);
+
+    game.objectSpeed = 0;
+    // Prevent ember from going backwards at screen edge.
+    if (position.x - 36 <= 0 && horizontalDirection < 0) {
+      velocity.x = 0;
+    }
+    // Prevent ember from going beyond half screen.
+    if (position.x + 64 >= game.size.x / 2 && horizontalDirection > 0) {
+      velocity.x = 0;
+      game.objectSpeed = -moveSpeed;
+    }
+    position += velocity * dt;
+    super.update(dt);
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is GroundBlock || other is PlatformBlock) {
+      if (intersectionPoints.length == 2) {
+        // Calcuate the collision normal and separation distance.
+        final mid = (intersectionPoints.elementAt(0) +
+                intersectionPoints.elementAt(1)) /
+            2;
+        final collisionNormal = absoluteCenter - mid;
+        final separationDistance = (size.x / 2) - collisionNormal.length;
+        collisionNormal.normalize();
+
+        // If collision normal is almost upwards,
+        // ember must be on ground.
+        if (fromAbove.dot(collisionNormal) > 0.9) {
+          isOnGround = true;
+        }
+        position += collisionNormal.scaled(separationDistance);
+      }
+    }
+    if (other is Star) {
+      other.removeFromParent();
+    }
+    if (other is WaterEnemy) {
+      hit();
+    }
+    super.onCollision(intersectionPoints, other);
+  }
+
+  void hit() {
+    if (!hitByEnemy) {
+      hitByEnemy = true;
+    }
+    add(
+      OpacityEffect.fadeOut(
+        EffectController(
+          alternate: true,
+          duration: 0.1,
+          repeatCount: 6,
+        ),
+      )..onComplete = () {
+          hitByEnemy = false;
+        },
     );
   }
 }
