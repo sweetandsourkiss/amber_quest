f9d89587b48ea5b7cb63fdce318281f2fbb7c88d
sweetandsourkiss <mooul0529@naver.com>
2024-12-16 18:06:51 +0900

    Add overlay map

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:amber_quest/ember_quest.dart';
+import 'package:amber_quest/overlays/game_over.dart';
+import 'package:amber_quest/overlays/main_menu.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    const GameWidget<EmberQuestGame>.controlled(
+    GameWidget<EmberQuestGame>.controlled(
       gameFactory: EmberQuestGame.new,
+      overlayBuilderMap: {
+        'MainMenu': (_, game) => MainMenu(game: game),
+        'GameOver': (_, game) => GameOver(game: game),
+      },
+      initialActiveOverlays: const ['MainMenu'],
     ),
   );
 }
